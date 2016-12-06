688c5ece9cf585d1bd459c904602fdb2f122e340
Manuel Morales <manuelmorales@gmail.com>
2016-12-06 15:32:17 +0000

    Added plug

--- lib/redirector.ex
+++ lib/redirector.ex
@@ -1,2 +1,15 @@
 defmodule Redirector do
+  defmodule PlugConnector do
+    import Plug.Conn
+
+    def init(options) do
+      options
+    end
+
+    def call(conn, _opts) do
+      conn
+      |> put_resp_content_type("text/plain")
+      |> send_resp(200, "Hello world")
+    end
+  end
 end
