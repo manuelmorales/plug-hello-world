1bf2a4e90c2a4b0fb45f53cebd46327d55188f7b
Manuel Morales <manuelmorales@gmail.com>
2016-12-10 11:39:19 +0000

    It's alive!

--- lib/redirector.ex
+++ lib/redirector.ex
@@ -1,15 +1,36 @@
 defmodule Redirector do
+  defmodule App do
+    use Application
+
+    def start(_type, _args) do
+      import Supervisor.Spec, warn: false
+
+      children = [
+        worker(Redirector.PlugConnector, [])
+      ]
+
+      opts = [strategy: :one_for_one, name: Redirector.Supervisor]
+      Supervisor.start_link(children, opts)
+    end
+  end
+
   defmodule PlugConnector do
     import Plug.Conn
 
     def init(options) do
+      IO.puts "Starting connector"
       options
     end
 
     def call(conn, _opts) do
+      IO.puts "Calling connector"
       conn
       |> put_resp_content_type("text/plain")
       |> send_resp(200, "Hello world")
+    end
+
+    def start_link do
+      {:ok, _} = Plug.Adapters.Cowboy.http Redirector.PlugConnector, []
     end
   end
 end
--- mix.exs
+++ mix.exs
@@ -10,22 +10,13 @@ defmodule Redirector.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :cowboy, :plug]]
+    [
+      applications: [:logger, :cowboy, :plug],
+      mod: {Redirector.App, []},
+    ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:cowboy, "~> 1.0.0"},
