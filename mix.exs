9d790370f0265f9a3873fdbdb67c892596e9b0ce
Manuel Morales <manuelmorales@gmail.com>
2016-12-29 11:56:09 +0000

    Not starting the app when running the tests

--- mix.exs
+++ mix.exs
@@ -2,12 +2,17 @@ defmodule HelloWorld.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :hello_world,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :hello_world,
+      version: "0.1.0",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      aliases: [
+        test: "test --no-start"
+      ],
+    ]
   end
 
   def application do
