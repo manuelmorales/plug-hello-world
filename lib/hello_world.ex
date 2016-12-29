539e85c5799387894e40a2130d7bb678465582e5
Manuel Morales <manuelmorales@gmail.com>
2016-12-29 11:51:21 +0000

    Forgot to remove it from the main module

--- lib/hello_world.ex
+++ lib/hello_world.ex
@@ -1,21 +1,2 @@
 defmodule HelloWorld do
-  defmodule PlugConnector do
-    import Plug.Conn
-
-    def init(options) do
-      IO.puts "Starting connector"
-      options
-    end
-
-    def call(conn, _opts) do
-      IO.puts "Calling connector"
-      conn
-      |> put_resp_content_type("text/plain")
-      |> send_resp(200, "Hello world")
-    end
-
-    def start_link do
-      {:ok, _} = Plug.Adapters.Cowboy.http HelloWorld.PlugConnector, []
-    end
-  end
 end
