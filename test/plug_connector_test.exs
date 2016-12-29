507171891a01fabb068b192c9dfea29fa330c8d7
Manuel Morales <manuelmorales@gmail.com>
2016-12-29 13:14:13 +0000

    Some refactor in the tests

--- test/plug_connector_test.exs
+++ test/plug_connector_test.exs
@@ -2,15 +2,16 @@ defmodule HelloWorldTest.PlugConnectorTest do
   use ExUnit.Case
   use Plug.Test
 
+  @connector HelloWorld.PlugConnector
+  @opts @connector.init([])
+
   test "has body Hello world" do
-    opts = HelloWorld.PlugConnector.init([])
-    conn = conn(:get, "/", "") |> HelloWorld.PlugConnector.call(opts)
+    conn = conn(:get, "/", "") |> @connector.call(@opts)
     assert conn.resp_body == "Hello world"
   end
 
   test "has status 200" do
-    opts = HelloWorld.PlugConnector.init([])
-    conn = conn(:get, "/", "") |> HelloWorld.PlugConnector.call(opts)
+    conn = conn(:get, "/", "") |> @connector.call(@opts)
     assert conn.status == 200
   end
 end
